3339fe58a0ae4a03e3c0e3a158bc1b9f1aaab724
Yamini G <yamireddy2004@gmail.com>
2026-08-10 23:50:28 +0530

    Update page.tsx

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -108,7 +108,6 @@ export default function SettingsPage() {
     return () => {
       cancelled = true;
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const [profileSavedAt, setProfileSavedAt] = useState<number | null>(null);
@@ -122,7 +121,6 @@ export default function SettingsPage() {
 
   useEffect(() => {
     profileForm.reset({ displayName: loadDisplayName() });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function handleProfileSave(values: ProfileSettingsFormValues) {
